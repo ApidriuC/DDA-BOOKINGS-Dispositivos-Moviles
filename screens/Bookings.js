5e0d666dd405b763bfda6006bc50e6639f1241d7
Andrés Campos <andresfloncam@gmail.com>
2021-10-19 12:53:57 -0500

    Botones de reserva funcionando

--- screens/Bookings.js
+++ screens/Bookings.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 
 const Bookings = () => {
     return (
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -60,50 +60,199 @@ const HomeScreen = () => {
           <View>
             <View style={styles.registerForm}>
               <View style={styles.columnas}>
-                <TouchableOpacity>
-                  <Image
-                    style={styles.CorteModerno}
-                    source={require("../assets/CorteModerno.jpeg")}
-                  />
-                  <Text style={styles.Textos}>Corte Moderno</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                  <Image
-                    style={styles.DepiladoBarba}
-                    source={require("../assets/DepiladoBarba.jpeg")}
-                  />
-                  <Text style={styles.Textos}>Depilado Barba</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                  <Image
-                    style={styles.CorteClasico}
-                    source={require("../assets/CorteClasico.jpeg")}
-                  />
-                  <Text style={styles.Textos}>Corte Clasico</Text>
-                </TouchableOpacity>
+                <View style={styles.columnas2}>
+                  <TouchableOpacity
+                    onPress={() => {
+                      navigation.navigate("Bookings");
+                    }}
+                  >
+                    <Image
+                      style={styles.CorteModerno}
+                      source={require("../assets/CorteModerno.jpeg")}
+                    />
+                    <Text style={styles.Textos}>Corte Moderno</Text>
+                  </TouchableOpacity>
+                  <Text style={styles.TextoInfo}>
+                    Nuestro corte moderno es un look que aporta un estilo limpio
+                    y diferente. Combina perfectamente con barbas abundantes.
+                  </Text>
+                  <View style={styles.registerForm}>
+                    <Text style={styles.TextoValor}>$ 15.000</Text>
+                    <TouchableOpacity
+                      style={styles.bottonReservas}
+                      onPress={() => {
+                        navigation.navigate("Bookings");
+                      }}
+                    >
+                      <Image
+                        style={styles.Reserva}
+                        source={require("../assets/bookings.png")}
+                      />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+                <View style={styles.columnas3}>
+                  <TouchableOpacity
+                    onPress={() => {
+                      navigation.navigate("Bookings");
+                    }}
+                  >
+                    <Image
+                      style={styles.DepiladoBarba}
+                      source={require("../assets/DepiladoBarba.jpeg")}
+                    />
+                    <Text style={styles.Textos}>Depilado Barba</Text>
+                  </TouchableOpacity>
+                  <Text style={styles.TextoInfo}>
+                    Afeitar una barba poblada requiere tiempo y cuidado cuando
+                    se afeitan se pueden producir cortes. Ofrecemos nuestro
+                    excelente servicio de corte con cuchilla con calidad y
+                    seguridad.
+                  </Text>
+                  <View style={styles.registerForm}>
+                    <Text style={styles.TextoValor}>$ 10.000</Text>
+                    <TouchableOpacity
+                      style={styles.bottonReservas}
+                      onPress={() => {
+                        navigation.navigate("Bookings");
+                      }}
+                    >
+                      <Image
+                        style={styles.Reserva}
+                        source={require("../assets/bookings.png")}
+                      />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+                <View style={styles.columnas4}>
+                  <TouchableOpacity
+                    onPress={() => {
+                      navigation.navigate("Bookings");
+                    }}
+                  >
+                    <Image
+                      style={styles.CorteClasico}
+                      source={require("../assets/CorteClasico.jpeg")}
+                    />
+                    <Text style={styles.Textos}>Corte Clasico</Text>
+                  </TouchableOpacity>
+                  <Text style={styles.TextoInfo}>
+                    El corte clásico se caracteriza por ser casi completamente
+                    simétrico y de una longitud corta o media, con un ligero
+                    desvanecido desde la altura de las sienes hasta la nuca.
+                  </Text>
+                  <View style={styles.registerForm}>
+                    <Text style={styles.TextoValor}>$ 17.000</Text>
+                    <TouchableOpacity
+                      style={styles.bottonReservas}
+                      onPress={() => {
+                        navigation.navigate("Bookings");
+                      }}
+                    >
+                      <Image
+                        style={styles.Reserva}
+                        source={require("../assets/bookings.png")}
+                      />
+                    </TouchableOpacity>
+                  </View>
+                </View>
               </View>
               <View style={styles.columnas}>
-                <TouchableOpacity>
-                  <Image
-                    style={styles.MascarillaNegra}
-                    source={require("../assets/MascarillaNegra.jpeg")}
-                  />
-                  <Text style={styles.Textos}>Mascarilla Negra</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                  <Image
-                    style={styles.BarbaCompleta}
-                    source={require("../assets/BarbaCompleta.jpeg")}
-                  />
-                  <Text style={styles.Textos}>Barba Completa</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                  <Image
-                    style={styles.Perfilado}
-                    source={require("../assets/Perfilado.jpeg")}
-                  />
-                  <Text style={styles.Textos}>Perfilado</Text>
-                </TouchableOpacity>
+                <View style={styles.columnas5}>
+                  <TouchableOpacity
+                    onPress={() => {
+                      navigation.navigate("Bookings");
+                    }}
+                  >
+                    <Image
+                      style={styles.MascarillaNegra}
+                      source={require("../assets/MascarillaNegra.jpeg")}
+                    />
+                    <Text style={styles.Textos}>Mascarilla Negra</Text>
+                  </TouchableOpacity>
+                  <Text style={styles.TextoInfo}>
+                    Esta mascarilla negra contiene extractos de canela, flor de
+                    loto, geranio y aceite de camelia. Ideal para espinillas y
+                    puntos negros.
+                  </Text>
+                  <View style={styles.registerForm}>
+                    <Text style={styles.TextoValor}>$ 10.000</Text>
+                    <TouchableOpacity
+                      style={styles.bottonReservas2}
+                      onPress={() => {
+                        navigation.navigate("Bookings");
+                      }}
+                    >
+                      <Image
+                        style={styles.Reserva}
+                        source={require("../assets/bookings.png")}
+                      />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+                <View style={styles.columnas6}>
+                  <TouchableOpacity
+                    onPress={() => {
+                      navigation.navigate("Bookings");
+                    }}
+                  >
+                    <Image
+                      style={styles.BarbaCompleta}
+                      source={require("../assets/BarbaCompleta.jpeg")}
+                    />
+                    <Text style={styles.Textos}>Barba Completa</Text>
+                  </TouchableOpacity>
+                  <Text style={styles.TextoInfo}>
+                    Realizamos el arreglo, perfilamiento, y corte completo a
+                    gusto de cada cliente, este corte se realiza en toda la
+                    barba.
+                  </Text>
+                  <View style={styles.registerForm}>
+                    <Text style={styles.TextoValor}>$ 20.000</Text>
+                    <TouchableOpacity
+                      style={styles.bottonReservas2}
+                      onPress={() => {
+                        navigation.navigate("Bookings");
+                      }}
+                    >
+                      <Image
+                        style={styles.Reserva}
+                        source={require("../assets/bookings.png")}
+                      />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+                <View style={styles.columnas6}>
+                  <TouchableOpacity
+                    onPress={() => {
+                      navigation.navigate("Bookings");
+                    }}
+                  >
+                    <Image
+                      style={styles.Perfilado}
+                      source={require("../assets/Perfilado.jpeg")}
+                    />
+                    <Text style={styles.Textos}>Perfilado</Text>
+                  </TouchableOpacity>
+                  <Text style={styles.TextoInfo}>
+                    Perfilado de la barba con cuchilla y correción de cortes
+                    irregulares con tijera.
+                  </Text>
+                  <View style={styles.registerForm2}>
+                    <Text style={styles.TextoValor}>$ 20.000</Text>
+                    <TouchableOpacity
+                      style={styles.bottonReservas2}
+                      onPress={() => {
+                        navigation.navigate("Bookings");
+                      }}
+                    >
+                      <Image
+                        style={styles.Reserva}
+                        source={require("../assets/bookings.png")}
+                      />
+                    </TouchableOpacity>
+                  </View>
+                </View>
               </View>
             </View>
           </View>
@@ -240,50 +389,41 @@ const styles = StyleSheet.create({
   registerForm: {
     flexDirection: "row",
   },
-  CorteModerno: {
-    marginLeft: 10,
+  registerForm2: {
     marginTop: 30,
+    flexDirection: "row",
+  },
+  CorteModerno: {
     width: 200,
     height: 200,
     alignItems: "center",
     borderRadius: 30,
   },
   MascarillaNegra: {
-    marginLeft: 10,
-    marginTop: 30,
-    marginRight: 10,
     width: 160,
     height: 300,
     alignItems: "center",
     borderRadius: 30,
   },
   DepiladoBarba: {
-    marginLeft: 10,
-    marginTop: 10,
     width: 200,
     height: 200,
     alignItems: "center",
     borderRadius: 30,
   },
   BarbaCompleta: {
-    marginLeft: 10,
-    marginTop: 10,
     width: 160,
     height: 200,
     alignItems: "center",
     borderRadius: 30,
   },
   CorteClasico: {
-    marginLeft: 10,
-    marginTop: 10,
     width: 200,
     height: 300,
     alignItems: "center",
     borderRadius: 30,
   },
   Perfilado: {
-    marginLeft: 10,
-    marginTop: 10,
     width: 160,
     height: 200,
     alignItems: "center",
@@ -300,6 +440,51 @@ const styles = StyleSheet.create({
   },
   columnas: {
     flexDirection: "column",
+  },
+  columnas2: {
+    marginLeft: 10,
+    borderRadius: 30,
+    marginTop: 30,
+    width: 200,
+    height: 370,
+    flexDirection: "column",
+    backgroundColor: "#E3E4E5",
+  },
+  columnas3: {
+    marginLeft: 10,
+    borderRadius: 30,
+    marginTop: 30,
+    width: 200,
+    height: 400,
+    flexDirection: "column",
+    backgroundColor: "#E3E4E5",
+  },
+  columnas4: {
+    marginLeft: 10,
+    borderRadius: 30,
+    marginTop: 30,
+    width: 200,
+    height: 500,
+    flexDirection: "column",
+    backgroundColor: "#E3E4E5",
+  },
+  columnas5: {
+    marginLeft: 10,
+    borderRadius: 30,
+    marginTop: 30,
+    width: 160,
+    height: 500,
+    flexDirection: "column",
+    backgroundColor: "#E3E4E5",
+  },
+  columnas6: {
+    marginLeft: 10,
+    borderRadius: 30,
+    marginTop: 30,
+    width: 160,
+    height: 385,
+    flexDirection: "column",
+    backgroundColor: "#E3E4E5",
   },
   User: {
     marginLeft: 330,
@@ -325,5 +510,42 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#FFFF",
+  },
+  TextoInfo: {
+    fontSize: 16,
+    marginTop: 10,
+    flexDirection: "column",
+    color: "#362B48",
+    fontFamily: "Roboto_300Light",
+    textAlign: "justify",
+  },
+  TextoValor: {
+    fontSize: 16,
+    marginTop: 20,
+    marginLeft: 15,
+    flexDirection: "column",
+    color: "#362B48",
+    fontFamily: "Roboto_300Light",
+    textAlign: "left",
+    fontWeight: "bold",
+  },
+  Reserva: {
+    width: 40,
+    height: 40,
+    alignItems: "center",
+  },
+  bottonReservas: {
+    marginLeft: 75,
+    marginTop: 8,
+    width: 50,
+    alignItems: "center",
+    alignContent: "center",
+  },
+  bottonReservas2: {
+    marginLeft: 36,
+    marginTop: 8,
+    width: 50,
+    alignItems: "center",
+    alignContent: "center",
   },
 });
